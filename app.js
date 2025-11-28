a8f3cfeb1393078e40713544dac0364c34135e69
Muhammad Sannan <sannansherzada7@gmail.com>
2025-11-28 18:40:29 +0500

    fix vercel deployment files

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv").config();
 
 const session = require("express-session");
 const mongodbsession = require("connect-mongodb-session")(session);
 const multer = require("multer");
-const url =
-  "mongodb+srv://sannansherzada7:khan@cluster0.2n1nfr6.mongodb.net/airbnb?retryWrites=true&w=majority&appName=Cluster0";
+const url = process.env.MONGO_URI;
 const authRouter = require("./routes/authRouter");
 const storeRouter = require("./routes/storeRouter");
 const hostRouter = require("./routes/hostRouter");
@@ -37,6 +37,7 @@ app.use("/uploads", express.static(path.join(rootdir, "uploads")));
 app.use("/host/uploads", express.static(path.join(rootdir, "uploads")));
 app.use("/store/uploads", express.static(path.join(rootdir, "uploads")));
 app.use("/homelist/uploads", express.static(path.join(rootdir, "uploads")));
+const PORT = process.env.PORT;
 const store = new mongodbsession({
   uri: url,
   collection: "sessions",
@@ -72,8 +73,8 @@ mongoose
   .connect(url)
   .then(() => {
     console.log("Connected to MongoDB using Mongoose");
-    app.listen(3000, () => {
-      console.log("Server http://localhost:3000");
+    app.listen(PORT, () => {
+      console.log("Server URL LINK");
     });
   })
   .catch((err) => {
